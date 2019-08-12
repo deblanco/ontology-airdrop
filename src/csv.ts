c86437e131c98d49f3ca12e5420eff6d9824f748
Daniel Blanco Parla <dblancoparla@gmail.com>
2019-08-13 01:00:27 +0200

    removed decimals check

--- src/csv.ts
+++ src/csv.ts
@@ -6,7 +6,7 @@ export function transformCsvToArray (path: string) {
 }
 
 export function isCsvFormatCorrect (arr: string[][]) {
-  const amountRegExp = /^[0-9]+(\.[0-9]+)?$/
+  const amountRegExp = /^[0-9]+$/
   const addressCheck = (address: string) =>
     address.length === 40 || address.length === 34
   return arr.every((row, i) => {
